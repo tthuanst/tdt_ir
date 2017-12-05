4f454c644bfa47de028e3bafa82ef1346863ecac
duythanh766 <thanhphamduy99@gmail.com>
2017-12-05 21:13:34 +0700

    edit stokenize add remove stopword function

--- stokenize/stokenize.py
+++ stokenize/stokenize.py
@@ -8,6 +8,8 @@
 #
 
 import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import wordpunct_tokenize
 
 example_vob = ["hello","world"]
 
@@ -21,6 +23,11 @@ def stokenize(sentence):
 	
     return nltk.word_tokenize(sentence)
 
+def stokenize_stop(sentence):
+	stop_words = set(stopwords.words('english'))
+	stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}'])
+	list_of_words = [i.lower() for i in wordpunct_tokenize(sentence) if i.lower() not in stop_words]
+	return list_of_words
 
 if __name__ == '__main__':
     #For test: ./stokenize.py

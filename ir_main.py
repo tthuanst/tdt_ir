9361ecded0ba85b2f8edb0d0827cfb81d82d97f2
Thuan <tthuanst@yahoo.com>
2017-12-19 23:31:06 +0700

    Done action items in first demo

--- ir_main.py
+++ ir_main.py
@@ -7,14 +7,17 @@
 # Lecturer: Le Anh Cuong
 #
 import os,sys
+import subprocess
 import numpy as np
+from datetime import datetime
 from stokenize import stokenize #Author: Thanh
 from indexing import inverted_index #Author: Hieu + Dai
 from searching import searching #Author: Thuan
 from correction import correction #Author: Phi + Thuy
 
-mode = ""
-indexed_data = {}
+data_path = ""
+indexed_simple = {}
+indexed_rank = {}
 #For run python from html
 #https://www.tutorialspoint.com/flask/flask_http_methods.htm
 from flask import Flask, render_template, request, redirect, url_for
@@ -22,94 +25,151 @@ import webbrowser as wb
 
 app = Flask(__name__)
 
-@app.route('/search_result/<input>')
-def search_result(input):
-    global mode,indexed_data
-    result = "Result:"+"<br>"
-    if mode == "simple":
-        for i in searching.simple_search(stokenize.stokenize_stop(input),indexed_data):
-            print(i)
-            result = result+str(i)+"<br>"
-    elif mode == "rank":
-        for i in searching.rank_search(stokenize.stokenize_stop(input),indexed_data):
-            print(i)
-            result = result+str(i)+"<br>"
+def display_result(input,searchMode):
+    global data_path,indexed_simple,indexed_rank
+    #Pattern for egrep to show a part of document
+    pattern = "\"("
+    query = []
+    for w in stokenize.stokenize_stop(input):
+        w = correction.correction(w.lower())
+        query.append(w)
+        pattern = pattern+w+"|"
+    pattern = pattern[:-1]+")\" "
+    #Show corrected sentences which is used to search
+    result = "Result simple search for <i><font color=\"red\">"+" ".join(query)+"</font></i><br><br>"
+    #Searching follow searchMode
+    if searchMode == "simple":
+        docs = searching.simple_search(query,indexed_simple)
+    elif searchMode == "rank":
+        docs = searching.rank_search(query,indexed_rank)
+    #For each document, show hyperlink to it and show a part of document
+    #TODO: hyperlink to local file cannot click to open
+    #Copy link and paste to address bar of browswer to open
+    for d in docs:
+        if type(d) is tuple:
+            d = d[0]
+        if os.path.exists(os.path.join(data_path,d)):
+            count = count + 1
+            result = result + "<br><a href=\"file:///{0}/{1}\" target=\"_blank\">{1}</a><br>".format(data_path,d)
+            cmd = "/bin/egrep -m 3 -i "+str(pattern)+str(os.path.join(data_path,d))
+            out = subprocess.check_output(cmd,stderr=subprocess.STDOUT,shell=True)
+            out.replace('\n','<br>')
+            result = result+out+"<br>"
+        else:
+            result = result+"<br>"+"<b>"+d+"</b><br>"
     return result
 
-@app.route('/search',methods = ['POST', 'GET'])
+@app.route('/search_simple/<input>')
+def search_simple(input):
+    return display_result(input,"simple")
+
+@app.route('/search_rank/<input>')
+def search_rank(input):
+    return display_result(input,"rank")
+
+@app.route('/search',methods = ['POST'])
 def search():
-    if request.method == 'POST':
+    if request.form['submit'] == "Simple search":
+        #Handle simple search button
         text = request.form['input']
-        return redirect(url_for('search_result',input = text))
-    else:
-        text = request.args.get('input')
-        return redirect(url_for('search_result',input = text))
+        print("Simple search: %s"%text)
+        return redirect(url_for('search_simple',input = text))
+    if request.form['submit'] == "Rank search":
+        #Handle rank search button
+        text = request.form['input']
+        print("Rank search: %s"%text)
+        return redirect(url_for('search_rank',input = text))
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+    #The first page of searching for query input
+    return render_template('index.html')
 
-
-def console(mode):
+#################################################
+def console():
+    #Console mode (to debug) can load 10 result per time
+    global indexed_simple,indexed_rank
     while(1):
         query = []
         text = raw_input("Search what: ")
         for w in stokenize.stokenize_stop(text):
+            #Correct each word in query
             w = correction.correction(w.lower())
             query.append(w)
         if len(query) == 0:
+            #All words in query are stop words
             print("No meaning word to search")
-        elif mode == "simple":
-            print(">>> Result: %s"%searching.simple_search(query,indexed_data))
-        elif mode == "rank":
-            print(">>> Result: %s"%searching.rank_search(query,indexed_data))
+        else:
+            #Print corrected sentence then search result
+            print("Searching \"%s\" ..."%(" ".join(query)))
+            print(">>> Result simple:")
+            print(searching.simple_search(query,indexed_simple))
+            print(">>> Result rank:")
+            count = 0
+            for d in searching.rank_search(query,indexed_rank):
+                print(d)
+                count = count + 1
+                if count%10 == 0:
+                    #To show only 10 result per time
+                    raw_input("Enter to load more")
 
+#Print usage of this program
+def usage():
+    print("Usage: %s search [console](optional)"%sys.argv[0])
+    print("Usage: %s index"%sys.argv[0])
+    print("Note: indexing new files in collections to update if database exists")
+    print("Note: indexing all files in collections to update if database not exists")
+    sys.exit(0)
+
+#For print error message then program's usage
+def error(message):
+    print(message)
+    usage()
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        mode = raw_input("Simple search or Rank search. Enter simple/rank: ")
-        gui = raw_input("Console or Webbrowser. Enter console/web: ")
-    elif len(sys.argv) == 3:
-        #First argument for mode selection
-        mode = sys.argv[1]
-        #Second argument for GUI selection
-        gui = sys.argv[2]
-    else:
-        print("Usage: %s or %s <mode> <gui>"%(sys.argv[0],sys.argv[0]))
-        sys.exit(1)
-
-    print("Information Retrieval mode=%s, app=%s"%(mode,app))
-    #Get current directory path which have this script
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    data_path = os.path.join(dir_path,"collections/ohsumed-all-docs")
-    print(data_path)
-    if mode == "simple":
-        if not os.path.exists('simple_index.npy'):
-            #indexed_data = searching.indexing(data_path)
-            #np.save('simple_index.npy',indexed_data)
-            indexed_data = inverted_index.indexing_basic(data_path)
-            np.save('simple_index.npy',indexed_data)
+    try:
+        #Get current directory path which have this script
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        data_path = os.path.join(dir_path,"collections/ohsumed-all-docs")
+        if sys.argv[1] == "search":
+            #No index database, return error. If exist, load it to global vars
+            if not (os.path.exists('simple_index.npy') and os.path.exists('rank_index.npy')):
+                error("No indexing database")
+            else:
+                indexed_simple = np.load('simple_index.npy').item()
+                indexed_rank = np.load('rank_index.npy').item()
+            if len(sys.argv) == 3 and sys.argv[2] == "console":
+                #For test with console: ./ir_main.py search console
+                console()
+            else:
+                #For test with browser (default): ./ir_main.py search
+                #CTRL + click to link to open it in default web browser
+                app.run(debug = True)
+        elif sys.argv[1] == "index":
+            #Index as new or updating index database (only new files will be indexed)
+            print("Collections: %s"%data_path)
+            try:
+                indexed_simple = np.load('simple_index.npy').item()
+                print("   ==> Simple indexing ... updating {0}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+            except:
+                indexed_simple = {}
+                print("   ==> Simple indexing ... start {0}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+            indexed_simple = searching.indexing(data_path,indexed_simple)
+            #indexed_simple = inverted_index.indexing_basic(data_path)
+            np.save('simple_index.npy',indexed_simple)
+            print("   ==> Simple indexing ... DONE {0}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+            try:
+                indexed_rank = np.load('rank_index.npy').item()
+                print("   ==> Rank indexing ... updating {0}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+            except:
+                indexed_rank = {}
+                print("   ==> Rank indexing ... start {0}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+            #indexed_rank = inverted_index.indexing_TF_IDF(data_path)
+            indexed_rank = searching.indexing_2(data_path,indexed_rank)
+            np.save('rank_index.npy',indexed_rank)
+            print("   ==> Rank indexing ... DONE {0}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
         else:
-            indexed_data = np.load('simple_index.npy').item()
-    elif mode == "rank":
-        if not os.path.exists('rank_index.npy'):
-            #indexed_data = inverted_index.indexing_TF_IDF(data_path)
-            #np.save('rank_index.npy',indexed_data)
-            indexed_data = searching.indexing_2(data_path)
-            np.save('rank_index.npy',indexed_data)
-        else:
-            indexed_data = np.load('rank_index.npy').item()
-    else:
-        print("Incorrect mode!")
-        sys.exit(1)
-
-    if gui == "web":
-        #For test with browser: ./ir_main.py <mode> web
-        #wb.open("http://127.0.0.1:5000/")
-        app.run(debug = True)
-    elif gui == "console":
-        #For test with console: ./ir_main.py <mode> console
-        console(mode)
-    else:
-        print("Incorrect GUI!")
-        sys.exit(1)
+            raise IndexError
+    except IndexError:
+        #Print usage if user input wrongly
+        usage()
--- searching/searching.py
+++ searching/searching.py
@@ -11,36 +11,51 @@ import os,re,math
 from stokenize import stokenize
 from collections import Counter
 
-def indexing(collections): #First draft in case of Hieu not yet done
-    vocabulary = set()
-    indexed_data = {}
+def indexing(collections,indexed_data): #First draft in case of Hieu not yet done
+    #Do indexing ...
+    #vocabulary = set()
+    count1 = 0
+    count2 = 0
+    if '__countDoc__' not in indexed_data.keys():
+        indexed_data['__countDoc__'] = []
     #All document paths in collection
     files = [os.path.join(collections,f) for f in os.listdir(collections)]
     for f in files:
         lines = open(f).readlines()
         f = os.path.basename(f) #Get document name only
+        if f in indexed_data['__countDoc__']:
+            count1 = count1 + 1
+            continue #Skip document already indexed
+        indexed_data['__countDoc__'].append(f)
+        count2 = count2 + 1
         for line in lines:
             for w in line.split(): #Simple stokenize, split by space
                 w = re.sub(r'/.*','',w) #This is for remove tag if use POS tagging data
-                vocabulary.add(w) #Build vocabulary, but it's not used
+                #vocabulary.add(w) #Build vocabulary, but it's not used
                 if indexed_data.has_key(w):
                     #Add document name to posting list of dictionary term 'w'
                     indexed_data[w].add(f)
                 else:
                     #Add first document name to posting list of dictionary term 'w'
                     indexed_data[w] = set([f])
+    print("\tSkip %d files already indexed, index %d new files"%(count1,count2))
     return indexed_data
 
-def indexing_2(collections):
+def indexing_2(collections,indexed_data):
     #Do indexing ...
-    indexed_data = {}
-    indexed_data['__countDoc__'] = 0
+    count1 = 0
+    count2 = 0
+    if '__countDoc__' not in indexed_data.keys():
+        indexed_data['__countDoc__'] = []
     files = [os.path.join(collections,f) for f in os.listdir(collections)]
     for f in files:
         lines = open(f).readlines()
         f = os.path.basename(f)
-        #Counting number of document in collections
-        indexed_data['__countDoc__'] = indexed_data['__countDoc__'] + 1
+        if f in indexed_data['__countDoc__']:
+            count1 = count1 + 1
+            continue #Skip document already indexed
+        indexed_data['__countDoc__'].append(f)
+        count2 = count2 + 1
         for line in lines:
             for w in stokenize.stokenize_stop(line):
                 if indexed_data.has_key(w):
@@ -53,33 +68,26 @@ def indexing_2(collections):
                     #Create dict with key(f) for term w
                     indexed_data[w] = dict()
                     indexed_data[w][f] = 1
+    print("\tSkip %d files already indexed, index %d new files"%(count1,count2))
     return indexed_data
 
 def rank_search(query,indexed_data):
-    result = None
-    for w in query:
-        if indexed_data.has_key(w):
-            if result is None:
-                result = set(indexed_data[w].keys())
-            else:
-                #Union 2 set of doc names
-                result = result | set(indexed_data[w].keys())
     score = {}
-    if result is not None:
-        for d in result:
-            for t in query:
-                if indexed_data[t].has_key(d):
-                    #Calculate weight for term in doc
-                    tf = indexed_data[t][d]
-                    idf = indexed_data['__countDoc__']/len(indexed_data[t].keys())
-                    weight = (1+math.log10(tf))*(math.log10(idf))
-                    if score.has_key(d):
-                        score[d] = score[d] + weight
-                    else:
-                        score[d] = weight
-        #Get top 10 highest score
+    for t in query:
+        if indexed_data.has_key(t):
+            for d in indexed_data[t].keys():
+                #Calculate weight for term in doc
+                tf = indexed_data[t][d]
+                idf = len(indexed_data['__countDoc__'])/len(indexed_data[t].keys())
+                weight = (1+math.log10(tf))*(math.log10(idf))
+                if score.has_key(d):
+                    score[d] = score[d] + weight
+                else:
+                    score[d] = weight
+    if len(score) != 0:
+        #Get top 100 highest score
         result = Counter(score)
-        return result.most_common(10)
+        return result.most_common(100)
     else:
         #For pretty printout
         return set(["Not found"])
